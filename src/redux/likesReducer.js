0ecde3e3719bed663dee57197e71aa17914c161c
Gegardus <stgegard@gmail.com>
2022-07-17 15:23:36 +0400

    Update readme in provider branch

--- src/redux/likesReducer.js
+++ src/redux/likesReducer.js
@@ -6,26 +6,22 @@ const initialState = {
 
 export const likesReducer = (state = initialState, action) => {
     console.log('likesReducer > ', action)
-
     // first reducer returns new state but for this moment we have not state changing mechanism    
     // then when action is cretaed in component, it comes to reducer 
     switch(action.type) {
-
         case INCREMENT:
             return{
                // as we can't change state manually, first we put current state
                ...state,
                likes: state.likes + 1
             }
-
         case DECREMENT:    
             return{
               // as we can't change state manually, first we put current state
               ...state,
               likes: state.likes - 1
             }
-
-            default:
-                return state;
-        }
+        default:
+            return state;
+    }
 }
