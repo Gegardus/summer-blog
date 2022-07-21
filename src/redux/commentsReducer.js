6891ef3307b4221d066ee4a492ee31fbdbcaf0c7
Gegardus <stgegard@gmail.com>
2022-07-21 20:42:40 +0400

    Add api with thunk and spin

--- src/redux/commentsReducer.js
+++ src/redux/commentsReducer.js
@@ -1,4 +1,4 @@
-import { COMMENT_CREATE, COMMENT_UPDATE, COMMENT_DELETE } from './types'
+import { COMMENT_CREATE, COMMENT_UPDATE, COMMENT_DELETE, COMMENTS_LOAD } from './types'
 
 const initialState = {
      comments: []
@@ -13,6 +13,17 @@ export const commentsReducer = (state = initialState, action) => {
                ...state,
                comments: [...state.comments, action.data]            
             }
+            case COMMENTS_LOAD:
+                const commentsNew = action.data.map(res => { //responce
+                  return {
+                    text: res.name,
+                    id: res.id
+                  }
+                })
+                return {
+                  ...state,
+                 comments: commentsNew
+                }    
         case COMMENT_UPDATE:
             const { data } = action;
             const { comments } = state;
@@ -27,21 +38,21 @@ export const commentsReducer = (state = initialState, action) => {
                 ...state,
                 comments: nextComments          
                 }   
-            case COMMENT_DELETE:
-                return (() => {
-                const { id } = action;
-                const { comments } = state;
-                const itemIndex = comments.findIndex(res => res.id === id);
+        case COMMENT_DELETE:
+            return (() => {
+            const { id } = action;
+            const { comments } = state;
+            const itemIndex = comments.findIndex(res => res.id === id);
                     
-                const nextComments = [
-                    ...comments.slice(0, itemIndex),                    
-                    ...comments.slice(itemIndex + 1)
-                    ];
-                    return{              
-                        ...state,
-                        comments: nextComments          
-                    }     
-                })();       
+            const nextComments = [
+                ...comments.slice(0, itemIndex),                    
+                ...comments.slice(itemIndex + 1)
+            ];
+            return{              
+                ...state,
+                comments: nextComments          
+            }     
+        })();       
         default:
             return state;
     }
